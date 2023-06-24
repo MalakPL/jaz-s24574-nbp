348ba550a6c6ea062941d0d7ee7da6553f8afa8d
s24574 (Malak) <szmajdakarol@Gmail.com>
2023-06-24 15:13:52 +0200

    Initial project

---
+++ src/test/java/pl/anovei/jazs24574nbp/JazS24574NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.anovei.jazs24574nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24574NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
